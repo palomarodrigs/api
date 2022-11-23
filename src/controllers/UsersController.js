db85ee473e5c84a498e8e7b6a8a50444eac03906
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2022-11-23 18:50:27 -0300

    initial commit

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,70 @@
+const { hash } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
+
+class UsersController {
+  async create(request, response) {
+    const { name, email, password } = request.body;
+
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, hashedPassword]
+    );
+
+    return response.status(201).json();
+  } // create - POST para criar um registro.
+
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if (!name) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(
+      `UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now'),
+      WHERE id = ?`,
+
+      [user.name, user.email, user.password, new Date(), id]
+    );
+  } // update - PUT para atualizar um registro.
+}
+
+module.exports = UsersController;
+
+// index - GET para listar vários registros.
+// show - GET para exibir um registro específico.
+
+// delete - DELETE para remover um registro.
