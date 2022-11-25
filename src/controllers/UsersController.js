c1abdc55463a5bef8f1fe3f8d74d989a8f169b5e
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2022-11-25 15:06:19 -0300

    :tada: back-end done

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
@@ -27,7 +27,7 @@ class UsersController {
   } // create - POST para criar um registro.
 
   async update(request, response) {
-    const { name, email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
@@ -46,25 +46,39 @@ class UsersController {
       throw new AppError("Este e-mail já está em uso.");
     }
 
-    user.name = name;
-    user.email = email;
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !old_password)
+      throw new AppError(
+        "Você precisa informar a senha antiga para definir a nova senha"
+      );
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.");
+      }
+
+      user.password = await hash(password, 8);
+    }
 
     await database.run(
       `UPDATE users SET
       name = ?,
       email = ?,
       password = ?,
+<<<<<<< Updated upstream
       updated_at = DATETIME('now'),
+=======
+      updated_at = DATETIME('now')
+>>>>>>> Stashed changes
       WHERE id = ?`,
 
-      [user.name, user.email, user.password, new Date(), id]
+      [user.name, user.email, user.password, id]
     );
   } // update - PUT para atualizar um registro.
 }
 
 module.exports = UsersController;
-
-// index - GET para listar vários registros.
-// show - GET para exibir um registro específico.
-
-// delete - DELETE para remover um registro.
